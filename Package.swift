1ac3d37f41c309a3beeba332e5ed49b3b9a2fb0a
Bas van Kuijck <bas@e-sites.nl>
2020-05-15 14:19:06 +0200

    Upped swift version

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
   name: "CommandLineKit",
+  platforms: [
+      .macOS(.v10_13)
+  ],
   products: [
       .library(name: "CommandLineKit", targets: ["CommandLineKit"]),
   ],
